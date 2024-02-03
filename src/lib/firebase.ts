3ec97a438b642e1c47cfebda90bbe7c6a1fee225
canklefark <canklefark@outlook.com>
2024-02-03 17:06:39 -0500

    add login auth

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,15 +1,16 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth';
-import { getStorage } from 'firebase/storage';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth, onAuthStateChanged, type User } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { writable } from "svelte/store";
 
 const firebaseConfig = {
-	apiKey: 'AIzaSyBp5vcQcEyOHpKeevssgxOVrzln5uIHXRw',
-	authDomain: 'cankletree.firebaseapp.com',
-	projectId: 'cankletree',
-	storageBucket: 'cankletree.appspot.com',
-	messagingSenderId: '29424998578',
-	appId: '1:29424998578:web:f2ff6d212d02ee91dd51ab'
+  apiKey: "AIzaSyBp5vcQcEyOHpKeevssgxOVrzln5uIHXRw",
+  authDomain: "cankletree.firebaseapp.com",
+  projectId: "cankletree",
+  storageBucket: "cankletree.appspot.com",
+  messagingSenderId: "29424998578",
+  appId: "1:29424998578:web:f2ff6d212d02ee91dd51ab",
 };
 
 //initialize firebase
@@ -17,3 +18,29 @@ export const app = initializeApp(firebaseConfig);
 export const db = getFirestore();
 export const auth = getAuth();
 export const storage = getStorage();
+
+function userStore() {
+  let unsubscribe: () => void;
+
+  if (!auth || !globalThis.window) {
+    console.warn("Auth is not initialized or not in browser");
+    const { subscribe } = writable<User | null>(null);
+    return {
+      subscribe,
+    };
+  }
+
+  const { subscribe } = writable(auth?.currentUser ?? null, (set) => {
+    unsubscribe = onAuthStateChanged(auth, (user) => {
+      set(user);
+    });
+
+    return () => unsubscribe();
+  });
+
+  return {
+    subscribe,
+  };
+}
+
+export const user = userStore();
--- src/routes/login/+layout.svelte
+++ src/routes/login/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+	import AnimatedRoute from '$lib/components/AnimatedRoute.svelte';
 </script>
 
 <nav class="flex justify-center my-6">
@@ -15,15 +16,17 @@
 		<a
 			class="step"
 			href="/login/photo"
-			class:step-primary={$page.route.id?.match(/username|photo/g)}
+			class:step-primary={$page.route.id?.includes('photo')}
 		>
 			Upload Photo
 		</a>
 	</ul>
 </nav>
 
-<main class="card w-4/6 bg-neutral text-neutral-content mx-auto">
-	<div class="card-body items-center text-center">
-		<slot />
-	</div>
-</main>
+<AnimatedRoute>
+	<main class="card w-4/6 bg-neutral text-neutral-content mx-auto">
+		<div class="card-body items-center text-center">
+			<slot />
+		</div>
+	</main>
+</AnimatedRoute>
